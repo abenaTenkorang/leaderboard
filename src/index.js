5030d694a99c076ab9078ca7ca23bdf6f18253c2
abenaTenkorang <abenagy16@gmail.com>
2023-01-02 20:43:18 +0000

    Hardcode js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-﻿import './style.css';
+import './style.css';
+
+const arr = [
+  {
+    name: 'abi',
+    score: 100,
+  },
+  {
+    name: 'jane',
+    score: 90,
+  },
+  {
+    name: 'james',
+    score: 100,
+  },
+  {
+    name: 'mimi',
+    score: 100,
+  },
+  {
+    name: 'jude',
+    score: 100,
+  },
+];
+const scoresBoard = document.getElementById('container');
+
+arr.forEach((e) => {
+  const score = document.createElement('p');
+  score.innerText = `${e.name}: ${e.score}`;
+  scoresBoard.append(score);
+});
